6b6165be776588328fc2b42933815cd5c983816b
Serg Nesterov <i.am.cust0dian@gmail.com>
2017-02-13 16:25:50 +0300

    Use ES2015 syntax
    Modern browsers support most of ES2015 standard, and it usually makes code
    shorter and easier to read, so it's a good idea to learn and use it
    
    More info:
    https://kangax.github.io/compat-table/es6/
    http://es6-features.org — overview of all new things
    
    Specifically used in this commit:
    http://es6-features.org/#Constants
    http://es6-features.org/#BlockScopedVariables
    http://es6-features.org/#ExpressionBodies
    http://es6-features.org/#StatementBodies
    http://es6-features.org/#PropertyShorthand

--- calc.js
+++ calc.js
@@ -1,34 +1,30 @@
-﻿var expression = (function() {
-    var OPERATORS = {
-        "-": function (a, b) { return a - b; },
-        "+": function (a, b) { return a + b; },
-        "*": function (a, b) { return a * b; },
-        "/": function (a, b) { return a / b; }
+﻿const expression = (() => {
+    const OPERATORS = {
+        "-": (a, b) => a - b,
+        "+": (a, b) => a + b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b
     };
-    var NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var FINALIZERS = ["=", "Enter"];
-    var symbols = [];
+    const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const FINALIZERS = ["=", "Enter"];
+    let symbols = [];
 
-    function append(symbol) {
-        symbols.push(symbol);
-    }
+    const append = (symbol) => symbols.push(symbol);
 
-    function clear() {
-        symbols = [];
-    }
+    const clear = () => symbols = [];
 
-    function calculate() {
-        var parts = toArray();
-        var result;
+    const calculate = () => {
+        const parts = toArray();
+        let result;
 
-        for (var i = 0; i < parts.length; i++) {
-            var part = parts[i];
+        for (let i = 0; i < parts.length; i++) {
+            const part = parts[i];
 
             if (isOperator(part)) {
                 // All operators are assumed to be binary
-                var isFirstOperator = i === 1;
+                const isFirstOperator = i === 1;
 
-                var a;
+                let a;
 
                 if (isFirstOperator) {
                     a = parts[i - 1];
@@ -36,41 +32,30 @@
                     a = result;
                 }
 
-                var b = parts[i + 1];
-                var associatedFn = OPERATORS[part];
+                const b = parts[i + 1];
+                const associatedFn = OPERATORS[part];
 
                 result = associatedFn(a, b);
             }
         }
 
         return result;
-    }
+    };
 
-    function isOperator(symbol) {
-        return Object.keys(OPERATORS).includes(symbol);
-    }
+    const isOperator = (symbol) => Object.keys(OPERATORS).includes(symbol);
+    const isNumber = (symbol) => NUMBERS.includes(symbol);
+    const isFinal = (symbol) => FINALIZERS.includes(symbol);
 
-    function isNumber(symbol) {
-        return NUMBERS.includes(symbol);
-    }
+    const toString = () => symbols.join("");
 
-    function isFinal(symbol) {
-        return FINALIZERS.includes(symbol);
-    }
-
-    function toString() {
-        return symbols.join("");
-    }
-
-    function toArray() {
-        var i;
-        var parts = [];
+    const toArray = () => {
+        const parts = [];
 
         // Concatenate consecutive non-operator symbols into their own parts
-        for (i = 0; i < symbols.length; i++) {
-            var symbol = symbols[i];
-            var previousPart = parts[parts.length - 1];
-            var isFirstSymbol = i === 0;
+        for (let i = 0; i < symbols.length; i++) {
+            const symbol = symbols[i];
+            const previousPart = parts[parts.length - 1];
+            const isFirstSymbol = i === 0;
 
             if (isOperator(symbol) || isOperator(previousPart) || isFirstSymbol) {
                 parts.push(symbol);
@@ -80,36 +65,34 @@
         }
 
         // Convert non-operator parts into numbers
-        for (i = 0; i < parts.length; i++) {
+        for (let i = 0; i < parts.length; i++) {
             if (!isOperator(parts[i])) {
                 parts[i] = Number(parts[i]);
             }
         }
 
         return parts;
-    }
+    };
 
     return {
-        append: append,
-        clear: clear,
-        calculate: calculate,
+        append,
+        clear,
+        calculate,
         helpers: {
-            isOperator: isOperator,
-            isNumber: isNumber,
-            isFinal: isFinal
+            isOperator,
+            isNumber,
+            isFinal
         },
-        toString: toString,
-        toArray: toArray
+        toString,
+        toArray
     };
-}());
+})();
 
 
-var calculator = (function(expression) {
-    function display(string) {
-        $("#display").val(string);
-    }
+const calculator = ((expression) => {
+    const display = (string) => $("#display").val(string);
 
-    function process(symbol) {
+    const process = (symbol) => {
         if (expression.helpers.isOperator(symbol)) {
             expression.append(symbol);
         }
@@ -123,32 +106,26 @@ var calculator = (function(expression) {
             display(expression.calculate());
             expression.clear();
         }
-    }
+    };
 
-    function clear() {
+    const clear = () => {
         display("");
         expression.clear();
-    }
+    };
 
     return {
-        process: process,
-        clear: clear
+        process,
+        clear
     };
-}(expression));
+})(expression);
 
 
-$(document).keydown(function (e) {
-    console.log(e.key);
-    calculator.process(e.key);
-});
+$(document).keydown((e) => calculator.process(e.key));
 
-$(document).ready(function () {
-    var $symbolButtons = $("#b0, #b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9, #bdel, #beq, #bdiv, #bplus, #btimes, #bsub");
-    var $clearButton = $("#bclr");
+$(document).ready(() => {
+    const $symbolButtons = $("#b0, #b1, #b2, #b3, #b4, #b5, #b6, #b7, #b8, #b9, #bdel, #beq, #bdiv, #bplus, #btimes, #bsub");
+    const $clearButton = $("#bclr");
 
-    $symbolButtons.on('click', function (e) {
-        calculator.process(e.target.innerHTML);
-    });
-
+    $symbolButtons.on('click', (e) => calculator.process(e.target.innerHTML));
     $clearButton.on('click', calculator.clear);
 });
