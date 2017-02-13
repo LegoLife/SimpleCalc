64709963f237008c81031606ad8635390e7a8229
Serg Nesterov <i.am.cust0dian@gmail.com>
2017-02-13 16:25:50 +0300

    Abstract out idea of an Expression

--- calc.js
+++ calc.js
@@ -6,11 +6,11 @@ $(document).keydown(function (e) {
     console.log(e.key);
 
     if (operators.includes(e.key)) {
-        allInputs.push(e.key);
+        appendToExpression(e.key);
     }
 
     if (numbers.includes(e.key)) {
-        allInputs.push(e.key);
+        appendToExpression(e.key);
         displayCurrentExpression();
     }
 
@@ -26,7 +26,7 @@ $(document).ready(function () {
         .click(function (e) {
             var symbol = e.target.innerHTML;
 
-            allInputs.push(symbol);
+            appendToExpression(symbol);
 
             if (numbers.includes(symbol)) {
                 displayCurrentExpression();
@@ -39,13 +39,14 @@ $(document).ready(function () {
 });
 
 function getAnswer() {
-    var i = indexOfOperator(allInputs, operators);
-    var operator = allInputs[i];
+    var expressionParts = getExpressionParts();
+    var i = indexOfOperator(expressionParts, operators);
+    var operator = expressionParts[i];
     var answer = "";
 
-    if (allInputs.length > 0 && operator != null) {
-        var firstNum = parseInt(allInputs.join("").split(operator)[0]);
-        var secondNum = parseInt(allInputs.join("").split(operator)[1]);
+    if (expressionParts.length > 0 && operator != null) {
+        var firstNum = parseInt(expressionParts.join("").split(operator)[0]);
+        var secondNum = parseInt(expressionParts.join("").split(operator)[1]);
 
         switch (operator) {
             case "+":
@@ -66,18 +67,34 @@ function getAnswer() {
     return answer;
 }
 
+function clearExpression() {
+    allInputs = [];
+}
+
+function appendToExpression(symbol) {
+    allInputs.push(symbol);
+}
+
+function getExpressionParts() {
+    return allInputs;
+}
+
+function getExpression() {
+    return allInputs.join("");
+}
+
 function clearDisplay() {
     display("");
-    allInputs = [];
+    clearExpression();
 }
 
 function displayResult() {
     display(getAnswer());
-    allInputs = [];
+    clearExpression();
 }
 
 function displayCurrentExpression() {
-    display(allInputs.join(""));
+    display(getExpression());
 }
 
 function display(string) {
